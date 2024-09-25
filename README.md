65a2f86b72691c71776eb01360f816c7eb53dbdf
Juliano <juliano.a.peres@gmail.com>
2024-09-25 07:57:03 -0300

    Monitoring traffic spped

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javscript
+
+test
---
+++ javascript_cursoemvideo/conditions/001.js
@@ -0,0 +1,7 @@
+var country = "France";
+console.log(`Living in ${country}.`);
+if (country != "Brazil") {
+  console.log("You are a Gringo!");
+} else {
+  console.log("You are Brazilian");
+}
